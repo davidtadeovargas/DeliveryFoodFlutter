53ad47ec840a33f3e3ba3aa8216678b7e5a049f9
davidtadeovargas <coritocorito@hotmail.com>
2020-07-09 08:22:49 -0600

    Initial commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\DavidTadeo.....Progr\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\DavidTadeo.....Progr\Documents\Carpeta de trabajo\Eduardo\Proyecto de Flutter\AppFlutter"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\DavidTadeo.....Progr\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=2.2.0"
+export "FLUTTER_BUILD_NUMBER=2.2.0"
